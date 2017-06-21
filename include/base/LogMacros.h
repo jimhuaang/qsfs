cd51d5a5438dde9eaec273a3435a00e2678a1abc
Jim Huang <jimhuang@yunify.com>
2017-06-21 17:11:55 +0800

    -Add unit test for logging -Refactory on logging system

--- include/base/LogMacros.h
+++ include/base/LogMacros.h
@@ -17,141 +17,196 @@
 #ifndef LOGMACROS_H_INCLUDED
 #define LOGMACROS_H_INCLUDED
 
+#include <glog/logging.h>
+
 #include <base/LogLevel.h>
 #include <base/Logging.h>
 
 #ifdef DISABLE_QS_LOGGING
-  #define Info(msg)
-  #define Warning(msg)
-  #define Error(msg)
-  #define Fatal(msg)
+#define Info(msg)
+#define Warning(msg)
+#define Error(msg)
+#define Fatal(msg)
 
-  #define InfoIf(condition, msg)
-  #define WarningIf(condition, msg)
-  #define ErrorIf(condition, msg)
-  #define FatalIf(condition, msg)
+#define InfoIf(condition, msg)
+#define WarningIf(condition, msg)
+#define ErrorIf(condition, msg)
+#define FatalIf(condition, msg)
 
-  #define DebugInfo(msg)
-  #define DebugWarning(msg)
-  #define DebugError(msg)
-  #define DebugFatal(msg)
+#define DebugInfo(msg)
+#define DebugWarning(msg)
+#define DebugError(msg)
+#define DebugFatal(msg)
 
-  #define DebugInfoIf(condition, msg)
-  #define DebugWarningIf(condition, msg)
-  #define DebugErrorIf(condition, msg)
-  #define DebugFatalIf(condition, msg)
+#define DebugInfoIf(condition, msg)
+#define DebugWarningIf(condition, msg)
+#define DebugErrorIf(condition, msg)
+#define DebugFatalIf(condition, msg)
 
 #else  //! DISABLE_QS_LOGGING
-  #define Info(msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessage(QS::Logging::LogLevel::Info, msg); \
-    } \
-  } \
-
-  #define Warning(msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessage(QS::Logging::LogLevel::Warn, msg); \
-    } \
-  } 
-
-  #define Error(msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessage(QS::Logging::LogLevel::Error, msg); \
-    } \
-  }
-  #define Fatal(msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessage(QS::Logging::LogLevel::Fatal, msg); \
-    } \
+// It seems the google INFO stream needs to be flushed.
+#define Info(msg)                                                              \
+  {                                                                            \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();                     \
+    if (log) {                                                                 \
+      LOG(INFO) << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Info) \
+                << msg;                                                        \
+      google::FlushLogFiles(google::INFO);                                     \
+    }                                                                          \
   }
 
-  #define InfoIf(condition, msg) { \
+#define Warning(msg)                                       \
+  {                                                        \
     QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessageIf(QS::Logging::LogLevel::Info, condition, msg); \
-    } \
-  } \
+    if (log) {                                             \
+      LOG(WARNING) << QS::Logging::GetLogLevelPrefix(      \
+                          QS::Logging::LogLevel::Warn)     \
+                   << msg;                                 \
+    }                                                      \
+  }
 
-  #define WarningIf(condition, msg) { \
+#define Error(msg)                                         \
+  {                                                        \
     QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessageIf(QS::Logging::LogLevel::Warn, condition, msg); \
-    } \
-  } \
+    if (log) {                                             \
+      LOG(ERROR) << QS::Logging::GetLogLevelPrefix(        \
+                        QS::Logging::LogLevel::Error)      \
+                 << msg;                                   \
+    }                                                      \
+  }
+#define Fatal(msg)                                         \
+  {                                                        \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
+    if (log) {                                             \
+      LOG(FATAL) << QS::Logging::GetLogLevelPrefix(        \
+                        QS::Logging::LogLevel::Fatal)      \
+                 << msg;                                   \
+    }                                                      \
+  }
 
-  #define ErrorIf(condition, msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessageIf(QS::Logging::LogLevel::Error, condition, msg); \
-    } \
-  } \
+#define InfoIf(condition, msg)                                           \
+  {                                                                      \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();               \
+    if (log) {                                                           \
+      LOG_IF(INFO, condition)                                            \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Info) \
+          << msg;                                                        \
+      google::FlushLogFiles(google::INFO);                               \
+    }                                                                    \
+  }
 
-  #define FatalIf(condition, msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->LogMessageIf(QS::Logging::LogLevel::Fatal, condition, msg); \
-    } \
-  } \
+#define WarningIf(condition, msg)                                        \
+  {                                                                      \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();               \
+    if (log) {                                                           \
+      LOG_IF(WARNING, condition)                                         \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Warn) \
+          << msg;                                                        \
+    }                                                                    \
+  }
 
-  #define DebugInfo(msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessage(QS::Logging::LogLevel::Info, msg); \
-    } \
-  } \
+#define ErrorIf(condition, msg)                                           \
+  {                                                                       \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();                \
+    if (log) {                                                            \
+      LOG_IF(ERROR, condition)                                            \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Error) \
+          << msg;                                                         \
+    }                                                                     \
+  }
 
-  #define DebugWarning(msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessage(QS::Logging::LogLevel::Warn, msg); \
-    } \
-  } \
+#define FatalIf(condition, msg)                                           \
+  {                                                                       \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();                \
+    if (log) {                                                            \
+      LOG_IF(FATAL, condition)                                            \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Fatal) \
+          << msg;                                                         \
+    }                                                                     \
+  }
 
-  #define DebugError(msg) { \
+// Log message in "debug mode" (i.e., there is no NDEBUG macro defined)
+#define DebugInfo(msg)                                     \
+  {                                                        \
     QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessage(QS::Logging::LogLevel::Error, msg); \
-    } \
-  } \
+    if (log) {                                             \
+      DLOG(INFO) << QS::Logging::GetLogLevelPrefix(        \
+                        QS::Logging::LogLevel::Info)       \
+                 << msg;                                   \
+      google::FlushLogFiles(google::INFO);                 \
+    }                                                      \
+  }
 
-  #define DebugFatal(msg) { \
+#define DebugWarning(msg)                                  \
+  {                                                        \
     QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessage(QS::Logging::LogLevel::Fatal, msg); \
-    } \
-  } \
+    if (log) {                                             \
+      DLOG(WARNING) << QS::Logging::GetLogLevelPrefix(     \
+                           QS::Logging::LogLevel::Warn)    \
+                    << msg;                                \
+    }                                                      \
+  }
 
-  #define DebugInfoIf(condition, msg) { \
+#define DebugError(msg)                                    \
+  {                                                        \
     QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessageIf(QS::Logging::LogLevel::Info, condition, msg); \
-    } \
-  } \
+    if (log) {                                             \
+      DLOG(ERROR) << QS::Logging::GetLogLevelPrefix(       \
+                         QS::Logging::LogLevel::Error)     \
+                  << msg;                                  \
+    }                                                      \
+  }
 
-  #define DebugWarningIf(condition, msg) { \
+#define DebugFatal(msg)                                    \
+  {                                                        \
     QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessageIf(QS::Logging::LogLevel::Warn, condition, msg); \
-    } \
-  } \
+    if (log) {                                             \
+      DLOG(FATAL) << QS::Logging::GetLogLevelPrefix(       \
+                         QS::Logging::LogLevel::Fatal)     \
+                  << msg;                                  \
+    }                                                      \
+  }
 
-  #define DebugErrorIf(condition, msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessageIf(QS::Logging::LogLevel::Error, condition, msg); \
-    } \
-  } \
+#define DebugInfoIf(condition, msg)                                      \
+  {                                                                      \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();               \
+    if (log) {                                                           \
+      DLOG_IF(INFO, condition)                                           \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Info) \
+          << msg;                                                        \
+      google::FlushLogFiles(google::INFO);                               \
+    }                                                                    \
+  }
 
-  #define DebugFatalIf(condition, msg) { \
-    QS::Logging::Log *log = QS::Logging::GetLogInstance(); \
-    if(log) { \
-      log->DebugLogMessageIf(QS::Logging::LogLevel::Fatal, condition, msg); \
-    } \
-  } \
+#define DebugWarningIf(condition, msg)                                   \
+  {                                                                      \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();               \
+    if (log) {                                                           \
+      DLOG_IF(WARNING, condition)                                        \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Warn) \
+          << msg;                                                        \
+    }                                                                    \
+  }
+
+#define DebugErrorIf(condition, msg)                                      \
+  {                                                                       \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();                \
+    if (log) {                                                            \
+      DLOG_IF(ERROR, condition)                                           \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Error) \
+          << msg;                                                         \
+    }                                                                     \
+  }
+
+#define DebugFatalIf(condition, msg)                                      \
+  {                                                                       \
+    QS::Logging::Log *log = QS::Logging::GetLogInstance();                \
+    if (log) {                                                            \
+      DLOG_IF(FATAL, condition)                                           \
+          << QS::Logging::GetLogLevelPrefix(QS::Logging::LogLevel::Fatal) \
+          << msg;                                                         \
+    }                                                                     \
+  }
 
 #endif  // DISABLE_QS_LOGGING
 
--- src/base/Logging.cpp
+++ src/base/Logging.cpp
@@ -21,14 +21,12 @@
 #include <memory>
 #include <mutex>
 
-#include <glog/logging.h>
 #include <base/LogLevel.h>
+#include <glog/logging.h>
 
 namespace {
 
-void InitializeGLog() {
-  google::InitGoogleLogging("QSFS");
-}
+void InitializeGLog() { google::InitGoogleLogging("QSFS"); }
 
 }  // namespace
 
@@ -37,7 +35,6 @@ namespace QS {
 namespace Logging {
 
 using std::once_flag;
-using std::string;
 using std::unique_ptr;
 
 static unique_ptr<Log> logInstance(nullptr);
@@ -51,78 +48,13 @@ void InitializeLogging(unique_ptr<Log> log) {
   });
 }
 
-void ShutdownLogging() {
-  if (logInstance) {
-    logInstance.reset();
-  }
-}
-
 Log* GetLogInstance() {
-  assert(logInstance);
-  if (!logInstance) {
-    InitializeLogging(unique_ptr<Log>(new DefaultLog(
-        LogLevel::Info, "")));  // TODO : replace with default log dir
-  }
+  std::call_once(logOnceFlag, [] {
+    logInstance =
+        unique_ptr<Log>(new DefaultLog(""));  // TODO : change to default path
+    InitializeGLog();
+  });
   return logInstance.get();
-}
-
-void Log::LogMessage(LogLevel logLevel, const string& msg) {
-  if (!(GetLogLevel() <= logLevel)) return;
-
-  string msgWithLogLevelPrefix = GetLogLevelPrefix(logLevel) + msg;
-  switch (logLevel) {
-    case LogLevel::Info:
-      LOG(INFO) << msgWithLogLevelPrefix;
-      break;
-    case LogLevel::Warn:
-      LOG(WARNING) << msgWithLogLevelPrefix;
-      break;
-    case LogLevel::Error:
-      LOG(ERROR) << msgWithLogLevelPrefix;
-      break;
-    case LogLevel::Fatal:
-      LOG(FATAL) << msgWithLogLevelPrefix;
-      break;
-    default:
-      assert(0);
-      break;
-  }
-}
-
-void Log::LogMessageIf(LogLevel logLevel, bool condition, const string& msg) {
-  if (condition) {
-    LogMessage(logLevel, msg);
-  }
-}
-
-void Log::DebugLogMessage(LogLevel logLevel, const string& msg) {
-  if (!(GetLogLevel() <= logLevel)) return;
-
-  string msgWithLogLevelPrefix = GetLogLevelPrefix(logLevel) + msg;
-  switch (logLevel) {
-    case LogLevel::Info:
-      DLOG(INFO) << msgWithLogLevelPrefix;
-      break;
-    case LogLevel::Warn:
-      DLOG(WARNING) << msgWithLogLevelPrefix;
-      break;
-    case LogLevel::Error:
-      DLOG(ERROR) << msgWithLogLevelPrefix;
-      break;
-    case LogLevel::Fatal:
-      DLOG(FATAL) << msgWithLogLevelPrefix;
-      break;
-    default:
-      assert(0);
-      break;
-  }
-}
-
-void Log::DebugLogMessageIf(LogLevel logLevel, bool condition,
-                            const string& msg) {
-  if (condition) {
-    DebugLogMessage(logLevel, msg);
-  }
 }
 
 void ConsoleLog::Initialize() { FLAGS_logtostderr = 1; }
