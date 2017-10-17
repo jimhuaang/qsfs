992e9883a335b5e072ca53efe8a4cc7b29f1d3eb
Jim Huang <jimhuang@yunify.com>
2017-10-17 10:06:39 +0800

    Support for unit test for Data::IOStream

--- test/StreamTest.cpp
+++ test/StreamTest.cpp
@@ -18,6 +18,8 @@
 
 #include <functional>
 #include <memory>
+#include <sstream>
+#include <string>
 #include <vector>
 
 #include "base/Logging.h"
@@ -27,12 +29,14 @@
 #include "data/StreamBuf.h"
 #include "data/StreamUtils.h"
 
-
 namespace QS {
 
 namespace Data {
 
+using QS::Data::IOStream;
 using QS::Data::StreamBuf;
+using std::string;
+using std::stringstream;
 using std::unique_ptr;
 using std::vector;
 using ::testing::Test;
@@ -47,9 +51,7 @@ void InitLog() {
       << "log instance is null";
 }
 
-void InitStreamWithNullBuffer() {
-  StreamBuf streamBuf(Buffer(), 1);
-}
+void InitStreamWithNullBuffer() { StreamBuf streamBuf(Buffer(), 1); }
 
 void InitStreamWithOverflowLength() {
   StreamBuf streamBuf(Buffer(new vector<char>(1)), 2);
@@ -58,9 +60,6 @@ void InitStreamWithOverflowLength() {
 class StreamBufTest : public Test {
  public:
   StreamBufTest() { InitLog(); }
-
-/*  protected:
-  StreamBuf *m_pStreamBuf = nullptr; */
 };
 
 TEST_F(StreamBufTest, DeathTestInitNull) {
@@ -80,14 +79,82 @@ TEST_F(StreamBufTest, Ctor) {
 TEST_F(StreamBufTest, PrivateFunc) {
   auto buf = vector<char>({'0', '1', '2'});
   StreamBuf streamBuf(Buffer(new vector<char>(buf)), buf.size() - 1);
-  ASSERT_TRUE(*(streamBuf.GetBuffer()) == buf);
+  EXPECT_TRUE(*(streamBuf.GetBuffer()) == buf);
 
-  ASSERT_TRUE(*(streamBuf.begin()) == '0');
-  ASSERT_TRUE(*(streamBuf.end()) == '2');
+  EXPECT_TRUE(*(streamBuf.begin()) == '0');
+  EXPECT_TRUE(*(streamBuf.end()) == '2');
 
   auto buf1 = streamBuf.ReleaseBuffer();
-  ASSERT_TRUE(*(buf1) == buf);
-  ASSERT_FALSE(streamBuf.GetBuffer());
+  EXPECT_TRUE(*(buf1) == buf);
+  EXPECT_FALSE(streamBuf.GetBuffer());
+}
+
+TEST(IOStreamTest, Ctor1) {
+  IOStream iostream(10);
+  auto buf = dynamic_cast<StreamBuf *>(iostream.rdbuf());
+  EXPECT_EQ(const_cast<const StreamBuf *>(buf)->GetBuffer()->size(), 10u);
+  auto buf1 = vector<char>(10);
+  EXPECT_TRUE(*(const_cast<const StreamBuf *>(buf)->GetBuffer()) == buf1);
+}
+
+TEST(IOStreamTest, Ctor2) {
+  auto buf = Buffer(new vector<char>({'0', '1', '2'}));
+  IOStream iostream(std::move(buf), 3);
+  auto streambuf = dynamic_cast<StreamBuf *>(iostream.rdbuf());
+  EXPECT_TRUE(*(const_cast<const StreamBuf *>(streambuf)->GetBuffer()) ==
+              vector<char>({'0', '1', '2'}));
+
+  auto buf1 = Buffer(new vector<char>({'0', '1', '2'}));
+  IOStream iostream1(std::move(buf1), 2);
+  auto streambuf1 = dynamic_cast<StreamBuf *>(iostream1.rdbuf());
+  EXPECT_TRUE(*(const_cast<const StreamBuf *>(streambuf1)->GetBuffer()) ==
+              vector<char>({'0', '1', '2'}));
+}
+
+TEST(IOStreamTest, Read1) {
+  IOStream stream(Buffer(new vector<char>({'0', '1', '2'})), 3);
+  stringstream ss;
+  ss << stream.rdbuf();
+  EXPECT_EQ(ss.str(), string("012"));
+
+  IOStream stream1(Buffer(new vector<char>({'0', '1', '2'})), 2);
+  stringstream ss1;
+  ss1 << stream1.rdbuf();
+  EXPECT_EQ(ss1.str(), string("01"));
+}
+
+TEST(IOStreamTest, Read2) {
+  IOStream stream(Buffer(new vector<char>({'0', '1', '2'})), 3);
+  stream.seekg(1, std::ios_base::beg);
+  stringstream ss;
+  ss << stream.rdbuf();
+  EXPECT_EQ(ss.str(), string("12"));
+}
+
+TEST(IOStreamTest, Write1) {
+  IOStream stream(Buffer(new vector<char>(3)), 3);
+  stringstream ss("012");
+  stream << ss.rdbuf();
+  auto streambuf = dynamic_cast<StreamBuf *>(stream.rdbuf());
+  EXPECT_TRUE(*(const_cast<const StreamBuf *>(streambuf)->GetBuffer()) ==
+              vector<char>({'0', '1', '2'}));
+
+  IOStream stream1(Buffer(new vector<char>(2)), 2);
+  stringstream ss1("012");
+  stream1 << ss1.rdbuf();
+  auto streambuf1 = dynamic_cast<StreamBuf *>(stream1.rdbuf());
+  EXPECT_TRUE(*(const_cast<const StreamBuf *>(streambuf1)->GetBuffer()) ==
+              vector<char>({'0', '1'}));
+}
+
+TEST(IOStreamTest, Write2) {
+  IOStream stream(Buffer(new vector<char>(3)), 3);
+  stringstream ss("012");
+  stream.seekp(1, std::ios_base::beg);
+  stream << ss.rdbuf();
+  auto streambuf = dynamic_cast<StreamBuf *>(stream.rdbuf());
+  EXPECT_TRUE(*(const_cast<const StreamBuf *>(streambuf)->GetBuffer()) ==
+              vector<char>({char(0), '0', '1'}));
 }
 
 }  // namespace Data
