6b54b214cf080e838325297792bef5968de00eea
Jim Huang <jimhuang@yunify.com>
2017-11-27 00:13:04 +0800

    Support download & install qingstor sdk

---
+++ cmake/qingstorsdk.cmake
@@ -0,0 +1,20 @@
+if (CMAKE_VERSION VERSION_LESS 3.2)
+    set(UPDATE_DISCONNECTED_IF_AVAILABLE "")
+else()
+    set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
+endif()
+
+include(cmake/DownloadInstallProject.cmake)
+
+setup_download_project(PROJ    qingstorsdk
+           GIT_REPOSITORY      git@git.internal.yunify.com:MorvenHuang/qingstor-sdk-c-and-cpp.git
+           GIT_TAG             master
+           ${UPDATE_DISCONNECTED_IF_AVAILABLE}
+)
+
+# Download and Install
+download_install_project(qingstorsdk)
+
+# Uninstall
+include(cmake/UninstallProject.cmake)
+setup_uninstall_project(qingstorsdk)
